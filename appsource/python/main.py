c5481eb61b7e6cc0cd694b91371f8312483573e7
Purushothkumar <developer2explore@gmail.com>
2024-05-27 20:27:21 +0530

    Local and Global Variable

--- appsource/python/main.py
+++ appsource/python/main.py
@@ -6,6 +6,33 @@ y = "John"
 print(x)
 print(y)
 
+# multiple variable declaration
+a, b, c = "Orange", "Banana", "Cherry"
+print(a)
+print(b)
+print(c)
+
+
+# define a variable as local
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+# define a variable as global
+y = "old variable"
+
+def myglobalfunc():
+    global y
+    print("declared variable is " + y)
+    y = "new variable"
+
+myglobalfunc()
+print("declared variable is " + y)
+
+
 
 # Get user input for name and color
 name = input("Enter your name: ")
